853dd1a9060df3a4f02be028e34dc81ed866be7a
Yurcell <chestayurcel@gmail.com>
2025-10-20 09:45:41 +0700

    menambah sebagian index.js file

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'chstmysql8',
+    database: 'mahasiswa',
+    port: 3308
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+});
